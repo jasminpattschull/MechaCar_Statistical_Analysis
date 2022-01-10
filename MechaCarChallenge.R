27a5004db16a7333efde8b83afcb1fdbc1f3a26f
jasminpattschull <90632470+jasminpattschull@users.noreply.github.com>
2022-01-09 19:15:09 -0600

    Del3 .R code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,14 +2,6 @@ library(dplyr)
 
 mechacar_mpg <- read.csv('MechaCar_mpg.csv') #import dataset
 
-lm(vehicle_length ~ mpg + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg) #create multiple linear regression model
+lm(vehicle_length ~ mpg, mechacar_mpg) #create linear model
 
-summary(lm(vehicle_length ~ mpg + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg)) #summarize linear model
-
-susp_coil <- read.csv('Suspension_Coil.csv') #import dataset
-
-total_summary <- susp_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI))
-
-lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI))
-
-
+summary(lm(vehicle_length ~ mpg,mechacar_mpg)) #summarize linear model
